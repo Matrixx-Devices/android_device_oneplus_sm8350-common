461ff887e94da0512831da3f307c361e3760f6e7
mukesh22584 <mks22584@gmail.com>
2023-08-19 17:25:52 +0300

    sm8350-common: overlay: Use MlkitBarcodeUI as default QR scanner
    - Every device isn't shiping Aperture cam. We need QR code scanner for all.

--- common.mk
+++ common.mk
@@ -320,6 +320,12 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Prebuilt packages
+PRODUCT_PACKAGES += \
+    MlkitBarcodeUI \
+    VisionBarcode \
+    QRCameraOverlay
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
