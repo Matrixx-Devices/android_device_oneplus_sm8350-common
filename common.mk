c3f13b2ae652c3cb10a152c75dc7f5ef32b2115a
mukesh22584 <mks22584@gmail.com>
2025-02-12 21:27:05 +0530

    sm8350-common: Add RemovePackages

--- common.mk
+++ common.mk
@@ -256,6 +256,10 @@ $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_en
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
