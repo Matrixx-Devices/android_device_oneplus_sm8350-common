d5993a5636e484dd788d97c6bc31acb8dad1a939
ShevT <evgeny.shishkov@gmail.com>
2025-11-08 10:24:48 +0300

    sm8350-common: Drop Hotword enrollment blobs
     * Fix 'Hey Google'
    
    Change-Id: I265e2c711b8296f16df435cb101a1498d89e99c5
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- common.mk
+++ common.mk
@@ -172,10 +172,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
-# Hotword enrollment
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
