dbd41a014570ba08470684f60d795cca6e783a1c
Jammy555 <prathap.venkata@gmail.com>
2026-03-15 09:02:17 +0530

    sm8350-common: Adjust cpu0 freq to avoid locking at max freq
    -changed min and max freq of cpu0 along with scheduling rated added
    -hook-up libperf sepolicy
    -utilise cgroup.json
    
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- common.mk
+++ common.mk
@@ -255,7 +255,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
-    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Power
 PRODUCT_PACKAGES += \
